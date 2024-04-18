215f195602f99ed8a8b312050c41c49c932a6484
GD <GD@DESKTOP-MDSFL1P>
2024-04-18 12:29:04 +0900

    main_Test클래스 생성

---
+++ gittest/src/com/gittest/main/Test.java
@@ -0,0 +1,5 @@
+package com.gittest.main;
+
+public class Test {
+
+}
--- gittest/src/gitTestContriller/GitTestController.java
+++ gittest/src/gitTestContriller/GitTestController.java
@@ -3,6 +3,5 @@ package gitTestContriller;
 public class GitTestController {
 	public void test() {
 		System.out.println("별찍기");
-		
 	}
 }
